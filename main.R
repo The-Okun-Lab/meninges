283c609fff1ebc6b1fbdfc8f3125f3bf73971046
Giulio Benedetti <giuliobene2000@gmail.com>
2023-11-26 18:48:53 +0200

    Check best size for images

--- main.R
+++ main.R
@@ -2,14 +2,15 @@ library(quarto)
 library(rmarkdown)
 
 strains <- c("APP", "C57")
+strains <- c("APP")
 
 for (strain in strains) {
   
   print(strain)
   
   quarto_render("meninges_report.qmd",
-                output_format = "md",
-                output_file = paste0(strain, ".md"),
+                output_format = "html",
+                output_file = paste0(strain, ".html"),
                 execute_params = list(str = strain))
   
 }
