a88a39a99a60aa33238963da3aabc97f30c02e79
Giulio Benedetti <giuliobene2000@gmail.com>
2024-01-23 19:04:11 +0200

    Add piecharts and text to plots

---
+++ globals.R
@@ -0,0 +1,37 @@
+library(ggplot2)
+library(patchwork)
+library(dplyr)
+library(png)
+library(grid)
+library(purrr)
+
+get_inset <- function(df, my_cols){
+  
+  sub_df <- df %>%
+    group_by(Phenotype) %>%
+    summarise(Amount = 100 * round(n() / nrow(.), 3))
+  
+  p <- ggplot(sub_df, aes(x = "", y = Amount, fill = Phenotype)) +
+    geom_bar(stat = "identity", width = 1) +
+    coord_polar("y", start = 0) +
+    scale_fill_manual(values = my_cols) +
+    geom_text(aes(label = paste0(Amount, "%")),
+              position = position_stack(vjust = 0.5), size = 2) +
+    theme_classic() +
+    theme(axis.title = element_blank(),
+          axis.line = element_blank(),
+          axis.text = element_blank(),
+          axis.ticks = element_blank(),
+          legend.position = "none")
+ 
+  return(p)
+}
+
+annotation_custom2 <- function(grob, xmin = -Inf, xmax = Inf, ymin = -Inf, ymax = Inf, data) {
+  
+  layer(data = data, stat = StatIdentity, position = PositionIdentity, 
+        geom = ggplot2:::GeomCustomAnn,
+        inherit.aes = TRUE, params = list(grob = grob, 
+                                          xmin = xmin, xmax = xmax, 
+                                          ymin = ymin, ymax = ymax))
+}
