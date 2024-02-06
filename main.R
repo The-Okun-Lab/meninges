7c755847e417aa59d4fc5e9597c2cfde5f2e2bb5
amitFruitman <amitfruitman@gmail.com>
2024-02-06 12:13:11 +0200

    change of color

--- main.R
+++ main.R
@@ -1,15 +1,15 @@
 library(quarto)
 library(rmarkdown)
 
-strains <- c("APP", "C57", "Tau", "WT", "APOE3", "APOE4")
+strains <- c("APP")
 
 for (strain in sort(strains)) {
   
   print(strain)
   
   quarto_render("meninges_report.qmd",
-                output_format = "md",
-                output_file = paste0(strain, ".md"),
+                output_format = "html",
+                output_file = paste0(strain, ".html"),
                 execute_params = list(str = strain))
   
 }
